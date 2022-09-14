a1bbe6dab558fb2af854c238e046cf502d94dc96
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-09-14 22:35:28 +0600

    fixing mistakes count

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ const typeController = (e) => {
   if (newLetterCorrect) {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
+    errorCount++
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
   }
 
@@ -75,6 +76,7 @@ const gameOver = () => {
   modalBackground.classList.toggle("hidden");
   // clear user text
   display.innerHTML = "";
+
   // make it inactive
   display.classList.add("inactive");
   // show result
